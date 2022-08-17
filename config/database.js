8e2cbc243bc1dd55ca4d46496b5a4c1c9cf4f67c
Nitin <nsolanki7788@gmail.com>
2022-08-17 14:11:27 +0530

    minor change

--- config/database.js
+++ config/database.js
@@ -32,6 +32,7 @@ module.exports = () => (
     "connections": {
       "default": {
         "connector": "mongoose",
+        
         "settings": {
 //           "uri": "mongodb://admin-naman_075:admin@cluster0-shard-00-00.uz7lk.mongodb.net:27017,cluster0-shard-00-01.uz7lk.mongodb.net:27017,cluster0-shard-00-02.uz7lk.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-2k9to0-shard-0&authSource=admin&retryWrites=true&w=majority"
              "uri": "mongodb://user:user@cluster0-shard-00-00.jdnli.mongodb.net:27017,cluster0-shard-00-01.jdnli.mongodb.net:27017,cluster0-shard-00-02.jdnli.mongodb.net:27017/nescii-blog?ssl=true&replicaSet=atlas-1316ir-shard-0&authSource=admin&retryWrites=true&w=majority"
